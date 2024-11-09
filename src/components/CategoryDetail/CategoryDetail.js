1db7e6a4dae69192d1c80ee3a25a74ed8f5d3017
mdalamin123a <md.alamindev90@gmail.com>
2024-11-09 11:02:36 +0600

    39-5-Update-Context-API-From-Any-Child-Component

--- src/components/CategoryDetail/CategoryDetail.js
+++ src/components/CategoryDetail/CategoryDetail.js
@@ -3,10 +3,11 @@ import { CategoryContext } from '../../App';
 
 const CategoryDetail = (props) => {
     const category = useContext(CategoryContext);
+    console.log(category);
     return (
         <div>
            <h3>This is your category Detail has</h3> 
-           <h4>Selected Category: {category}</h4>
+           <h4>Selected Category: {category[0]}</h4>
         </div>
     );
 };
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CategoryContext } from '../../App';
 
 const Shipment = () => {
+    const [category, setCategory] = useContext(CategoryContext);
+    console.log(setCategory);
     return (
         <div>
             <h1>This is Shipment</h1>
+            <button onClick={()=>setCategory(category + 1)}>Increment from Category</button>
         </div>
     );
 }
